6de5103c67576fe43989e807b6dfc5639a3528ed
Jake Antonio <jakemattantonio@gmail.com>
2021-11-01 11:31:59 +1100

    Added and tested playRound() successfully

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 console.log("Hello world!");
 
+//Computer's random selection of rock/paper/scissors
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice == 1) return console.log("Rock");
-    else if (choice == 2) return console.log("Paper");
-    else return console.log("Scissors");
+    if (choice == 1) return "ROCK";
+    else if (choice == 2) return "PAPER";
+    else return "SCISSORS";
 }
 
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection.toUpperCase()) {
+        case "ROCK":
+            if (computerSelection == "ROCK") return `It's a tie! Both chose rock!`;
+            else if (computerSelection == "PAPER") return `You lose! Paper beats rock!`;
+            else return `You win! Rock beats scissors!`;
+            break;
+        default: break;
+    }
+}
+
+const playerSelection = "rock";
 const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
